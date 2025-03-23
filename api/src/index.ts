97431339cb7eebb756834c2ef0b03afa3f7acd07
Sajin Shrestha <sajinshrestha@Sajins-MacBook-Air.local>
2025-03-23 18:49:39 -0400

    [FEAT]: Tanstack query

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,9 @@
-import express, { json, urlencoded, Request } from 'express';
-import productsRoutes from './routes/products/index.js';
-import authRoutes from './routes/auth/index.js';
-import ordersRoutes from './routes/orders/index.js';
-import stripeRoutes from './routes/stripe/index.js';
-
-import serverless from 'serverless-http';
+import express, { json, urlencoded, Request } from "express";
+import productsRoutes from "./routes/products/index.js";
+import authRoutes from "./routes/auth/index.js";
+import ordersRoutes from "./routes/orders/index.js";
+// import stripeRoutes from "./routes/stripe/index.js";
+import serverless from "serverless-http";
 
 const port = 3001;
 const app = express();
@@ -18,16 +17,16 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use('/products', productsRoutes);
-app.use('/auth', authRoutes);
-app.use('/orders', ordersRoutes);
-app.use('/stripe', stripeRoutes);
+app.use("/products", productsRoutes);
+app.use("/auth", authRoutes);
+app.use("/orders", ordersRoutes);
+// app.use("/stripe", stripeRoutes);
 
-if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === "dev") {
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
--- api/src/routes/stripe/index.ts
+++ api/src/routes/stripe/index.ts
@@ -1,14 +1,14 @@
-import { Router } from 'express';
-import { createPaymentIntent, getKeys, webhook } from './stripeController.js';
-import { verifyToken } from '../../middlewares/authMiddleware.js';
+// import { Router } from 'express';
+// import { createPaymentIntent, getKeys, webhook } from './stripeController.js';
+// import { verifyToken } from '../../middlewares/authMiddleware.js';
 
-const router = Router();
+// const router = Router();
 
-// Note: Guests can get the publishable key
-router.get('/keys', getKeys);
+// // Note: Guests can get the publishable key
+// router.get('/keys', getKeys);
 
-router.post('/payment-intent', verifyToken, createPaymentIntent);
+// router.post('/payment-intent', verifyToken, createPaymentIntent);
 
-router.post('/webhook', webhook);
+// router.post('/webhook', webhook);
 
-export default router;
+// export default router;
